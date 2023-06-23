4ac9ecc021d8484e8f5892f7369ea67fff3f9af0
Nicole Shimamoto <nicshimamoto@gmail.com>
2023-06-23 11:29:23 -0700

    Initial Commit

---
+++ Welcome/WelcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeApp.swift
+//  Welcome
+//
+//  Created by Nicole Shimamoto on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
